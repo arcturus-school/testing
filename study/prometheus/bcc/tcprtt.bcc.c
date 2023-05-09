1d9062fcfab280d87687823776394d33f11f85c9
ARCTURUS <1638330246@qq.com>
2023-05-09 16:38:07 +0800

    feat: A demo for prometheus

---
+++ study/prometheus/bcc/tcprtt.bcc.c
@@ -0,0 +1,46 @@
+#include <net/inet_sock.h>
+#include <uapi/linux/ptrace.h>
+
+struct data_t {
+    u64 ts;        // 当前时间戳
+    u64 rtt_ms;    // 往返延迟
+    u32 dest_ip;   // 目的地址
+    u32 src_ip;    // 源地址
+    u16 dest_port; // 目的端口
+    u16 src_port;  // 源端口
+};
+
+BPF_HASH(start, struct sock*, struct data_t);
+
+BPF_PERF_OUTPUT(events);
+
+int trace_tcp_send(struct pt_regs* ctx, struct sock* sk) {
+    if (sk == NULL) return 0;
+
+    struct data_t data = {};
+
+    data.ts        = bpf_ktime_get_ns();
+    data.src_ip    = sk->__sk_common.skc_rcv_saddr;
+    data.dest_ip   = sk->__sk_common.skc_daddr;
+    data.src_port  = sk->__sk_common.skc_num;
+    data.dest_port = sk->__sk_common.skc_dport;
+
+    start.update(&sk, &data);
+
+    return 0;
+}
+
+int trace_tcp_ack(struct pt_regs* ctx, struct sock* sk) {
+    struct data_t* datap = start.lookup(&sk);
+
+    if (datap == NULL) return 0;
+
+    u64 rtt_ns    = bpf_ktime_get_ns() /* ack 的时间 */ - datap->ts;
+    datap->rtt_ms = rtt_ns / 1000;
+
+    events.perf_submit(ctx, datap, sizeof(struct data_t));
+
+    start.delete(&sk);
+
+    return 0;
+}
---
+++ study/prometheus/bcc/tcprtt_exporter.py
@@ -0,0 +1,49 @@
+from prometheus_client import start_http_server, Gauge
+from socket import inet_ntop, AF_INET
+from bcc import BPF
+import struct
+
+# 加载 ebpf 程序
+b = BPF(src_file="tcprtt.bcc.c")
+
+# 插入探针
+b.attach_kprobe(event="tcp_sendmsg", fn_name="trace_tcp_send")
+b.attach_kprobe(event="tcp_ack", fn_name="trace_tcp_ack")
+
+# 定义 Prometheus 指标
+tcp_rtt_gauge = Gauge(
+    "tcp_rtt_ms",
+    "TCP RTT in microsecond",
+    [
+        "src_ip",
+        "dest_ip",
+        "src_port",
+        "dest_port",
+    ],
+)
+
+
+def ip_to_str(addr):
+    return inet_ntop(AF_INET, struct.pack("I", addr))
+
+
+def handle_event(cpu, data, size):
+    event = b["events"].event(data)
+
+    tcp_rtt_gauge.labels(
+        src_ip=ip_to_str(event.src_ip),
+        dest_ip=ip_to_str(event.dest_ip),
+        src_port=event.src_port,
+        dest_port=event.dest_port,
+    ).set(event.rtt_ms)
+
+
+b["events"].open_perf_buffer(handle_event)
+
+if __name__ == "__main__":
+    start_http_server(8000)
+
+    print("Server is start at http://127.0.0.1:8000")
+
+    while True:
+        b.perf_buffer_poll()
