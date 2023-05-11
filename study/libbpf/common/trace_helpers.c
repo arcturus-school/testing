68b7a52c47b9f2600ac8d7371c674fd451b6bf28
ARCTURUS <1638330246@qq.com>
2023-05-12 00:41:41 +0800

    feat: Add find_ksym_by_name

--- study/libbpf/common/trace_helpers.c
+++ study/libbpf/common/trace_helpers.c
@@ -1,4 +1,5 @@
 #include "trace_helpers.h"
+#include <stdio.h>
 #include <time.h>
 
 unsigned long long get_ktime_ns(void) {
@@ -6,4 +7,30 @@ unsigned long long get_ktime_ns(void) {
 
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return ts.tv_sec * NSEC_PER_SEC + ts.tv_nsec;
+}
+
+bool find_ksym_by_name(const char* name) {
+
+    FILE* fp = NULL;
+    char  cmd[CMD_LEN];
+    char  buf[SYM_LEN] = "\0";
+
+    sprintf(cmd, "FIND_KSYM=`echo %s |awk -F'kprobe_' '{print $2}'`; cat /proc/kallsyms |grep -w $FIND_KSYM", name);
+
+    fp = popen(cmd, "r");
+
+    if (fp == NULL) {
+        return false;
+    }
+
+    bool  has = false;
+    char* f   = fgets(buf, SYM_LEN, fp);
+
+    if (f != NULL) {
+        has = true;
+    }
+
+    pclose(fp);
+
+    return has;
 }
--- study/libbpf/common/trace_helpers.h
+++ study/libbpf/common/trace_helpers.h
@@ -1,8 +1,16 @@
 #ifndef __TRACE_HELPERS_H
 #define __TRACE_HELPERS_H
 
+#include <stdbool.h>
+
 #define NSEC_PER_SEC 1000000000ULL
 
+#define CMD_LEN 4096
+
+#define SYM_LEN 1024
+
 unsigned long long get_ktime_ns(void);
+
+bool find_ksym_by_name(const char*);
 
 #endif
