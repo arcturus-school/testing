a124f85485ef92cf0cf3a7f76a1dc72e3f16cfab
ARCTURUS <1638330246@qq.com>
2023-05-11 20:51:57 +0800

    feat: Format code

--- study/libbpf/hello/hello.bpf.c
+++ study/libbpf/hello/hello.bpf.c
@@ -3,8 +3,8 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
-SEC("tp/syscalls/sys_enter_write") int handle_tp(void* ctx) {
-
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void* ctx) {
     bpf_printk("hello world.\n");
 
     return 0;
