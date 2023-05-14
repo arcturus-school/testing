0f6d434bb0afd4baba1aeb5e2f94474436815aff
ARCTURUS <1638330246@qq.com>
2023-05-14 22:04:47 +0800

    feat: Add const to parameter

--- src/server/utils/file.hpp
+++ src/server/utils/file.hpp
@@ -1,13 +1,16 @@
-#pragma once
+#ifndef _FILE_H
+#define _FILE_H
 
 #include <filesystem>
 #include <string>
 
 // 获取绝对路径
-std::string get_absolute_path(std::string& file);
+std::string get_absolute_path(const std::string& file);
 
 // 判断文件是否存在
 bool exists(const std::string& file);
 
 // 获取当前工作目录
 std::string current_path();
+
+#endif
