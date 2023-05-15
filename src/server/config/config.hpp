7e704f2df1e1581ccd3754f2a7cad5fc6c67f505
ARCTURUS <1638330246@qq.com>
2023-05-15 14:12:27 +0800

    feat: Move struct Program

--- src/server/config/config.hpp
+++ src/server/config/config.hpp
@@ -1,6 +1,7 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
+#include "../exporter/exporter.hpp"
 #include "../utils/file.hpp"
 #include "../utils/log.hpp"
 
@@ -8,11 +9,6 @@
 #include <map>
 #include <string>
 #include <yaml-cpp/yaml.h>
-
-struct Program {
-    bpf_object* object;
-    YAML::Node  metrics;
-};
 
 // 读取配置文件
 error_t read_config();
