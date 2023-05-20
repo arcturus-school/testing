e7e0c30a88ed2116218a8e6f36451b426fa2f384
ARCTURUS <1638330246@qq.com>
2023-05-20 19:59:13 +0800

    feat: Add judgment for clone failure

--- src/server/run.sh
+++ src/server/run.sh
@@ -4,9 +4,14 @@ sudo apt-get update
 
 # 安装 libbpf 环境
 sudo apt install clang cmake llvm git libelf-dev libfl-dev make \
-	pkg-config curl libcurl4-openssl-dev gcc-multilib -y 
+  pkg-config curl libcurl4-openssl-dev gcc-multilib -y 
 
 git clone https://github.com/libbpf/libbpf.git
+
+if [ ! -d "libbpf" ]; then
+  echo "clone libbpf failed."
+  exit
+fi
 
 cd libbpf/src
 
@@ -19,6 +24,11 @@ sudo make install
 
 git clone --recurse-submodules https://github.com/libbpf/bpftool.git
 
+if [ ! -d "bpftool" ]; then
+  echo "clone bpftool failed."
+  exit
+fi
+
 cd bpftool
 
 git submodule update --init
@@ -27,7 +37,6 @@ cd src
 
 sudo make install
 
-# 这步可能不需要, 如果运行程序时出现 
 # error while loading shared libraries: libbpf.so.1: cannot open shared object file: No such file or directory
 # 可能是 libbpf.so.1 被装到 /usr/lib64 下了, 而非 /usr/lib 里, 
 # 因此将新的共享文件目录 /usr/lib64 加入 /etc/ld.so.conf 里
@@ -38,6 +47,11 @@ sudo apt install g++
 
 # 安装 yaml-cpp
 git clone https://github.com/jbeder/yaml-cpp.git
+
+if [ ! -d "yaml-cpp" ]; then
+  echo "clone yaml-cpp failed."
+  exit
+fi
 
 cd yaml-cpp
 
@@ -50,13 +64,16 @@ cmake --build .
 sudo cmake --install .
 
 # 安装 Prometheus-CPP
-git clone https://github.com/jupp0r/prometheus-cpp.git
+git clone --recurse-submodules https://github.com/jupp0r/prometheus-cpp.git
+
+if [ ! -d "prometheus-cpp" ]; then
+  echo "clone prometheus-cpp failed."
+  exit
+fi
 
 cd prometheus-cpp
 
-# 更新子模块
-git submodule init
-git submodule update
+git submodule update --init
 
 mkdir build; cd build
 
@@ -70,6 +87,11 @@ sudo ldconfig
 
 # 安装 Prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.44.0/prometheus-2.44.0.linux-amd64.tar.gz
+
+if [ ! "prometheus-2.44.0.linux-amd64.tar.gz" ]; then
+  echo "prometheus-2.44.0.linux-amd64.tar.gz download failed."
+  exit
+fi
 
 tar -zxvf prometheus-2.44.0.linux-amd64.tar.gz
 
@@ -89,3 +111,4 @@ sudo ./ecli -v -c config.yaml &
 ./prometheus-2.44.0.linux-amd64/prometheus --config.file=prometheus.yml &
 
 # 访问 localhost:9090
+echo "Server is running at localhost:9090"
