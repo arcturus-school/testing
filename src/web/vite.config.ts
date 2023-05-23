b70304d9d47d7a40409f04319fb0508d2630debc
ARCTRUS <1638330246@qq.com>
2023-05-23 12:16:19 +0800

    chore: Add on-demand components auto importing

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@src': resolve(__dirname, 'src'),
