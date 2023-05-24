08c11be45754b92fe4c510946983eb514e858256
ARCTRUS <1638330246@qq.com>
2023-05-24 14:52:18 +0800

    feat: Add app.vue

--- src/web/src/app.vue
+++ src/web/src/app.vue
@@ -1,7 +1,61 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div></div>
+  <a-config-provider :locale="locale">
+    <a-layout>
+      <a-layout-header>
+        <a-space :size="20">
+          <e-metrics />
+          <e-chart-select />
+        </a-space>
+        <a-space :size="5">
+          <e-time-select />
+          <a-button @click="store.refreshData">
+            <template #icon>
+              <sync-outlined />
+            </template>
+          </a-button>
+        </a-space>
+      </a-layout-header>
+      <a-layout-content>
+        <e-chart />
+      </a-layout-content>
+    </a-layout>
+  </a-config-provider>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import EMetrics from '@components/e-metrics.vue';
+import EChartSelect from '@components/e-chart-select.vue';
+import EChart from '@components/e-chart.vue';
+import ETimeSelect from '@components/e-time-select.vue';
+import locale from 'ant-design-vue/es/locale/zh_CN';
+import { SyncOutlined } from '@ant-design/icons-vue';
+import { useStore } from '@src/store';
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh';
+
+dayjs.locale('zh-cn');
+
+const store = useStore();
+
+store.getLabels();
+</script>
+
+<style scoped lang="scss">
+.ant-layout {
+  min-height: 100vh;
+
+  .ant-layout-header {
+    background: #fff;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .ant-layout-content {
+    flex: 1;
+    display: flex;
+    padding: 10px 50px;
+    background-color: #fff;
+  }
+}
+</style>
