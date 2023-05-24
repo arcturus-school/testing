3bfaea5aaf00094748a5e4ad5f6eaaa271437488
ARCTRUS <1638330246@qq.com>
2023-05-24 12:47:47 +0800

    feat: Add warn

--- src/web/src/utils/log.ts
+++ src/web/src/utils/log.ts
@@ -3,3 +3,9 @@ export const log = (msg: any, ...options: any[]) => {
     console.log(msg, ...options);
   }
 };
+
+export const warn = (msg: any, ...options: any[]) => {
+  if (import.meta.env.DEV) {
+    console.warn(msg, ...options);
+  }
+};
