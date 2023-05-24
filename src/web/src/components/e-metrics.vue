9679e2a514f27d2ccda95e5a806a8dbdc0f6ec5f
ARCTRUS <1638330246@qq.com>
2023-05-24 15:24:45 +0800

    feat: Add metric select component

---
+++ src/web/src/components/e-metrics.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="metrics-select-wrapper">
+    <span class="metrics-label">指标:</span>
+    <a-select
+      class="metrics-select"
+      :options="labels"
+      v-model:value="label"
+      @change="changeHandler"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from '@src/store';
+import { log } from '@utils/log';
+import { storeToRefs } from 'pinia';
+
+const store = useStore();
+
+const { labels, label } = storeToRefs(store);
+
+const changeHandler = function (value: string) {
+  log(`select label: ${value}...`);
+
+  store.getMetricData();
+};
+</script>
+
+<style scoped lang="scss">
+.metrics-select-wrapper {
+  .metrics-select {
+    min-width: 220px;
+  }
+
+  .metrics-label {
+    margin-right: 10px;
+  }
+}
+</style>
