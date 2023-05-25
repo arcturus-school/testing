4d340f82c45d07c02dde54def523f1e1ebe7ec98
ARCTRUS <1638330246@qq.com>
2023-05-25 18:53:25 +0800

    feat: Add drawCounter

--- src/web/src/utils/draw.ts
+++ src/web/src/utils/draw.ts
@@ -1,22 +1,27 @@
 import { Chart } from '@antv/g2';
 import { log } from '@utils/log';
 import { Renderer } from '@antv/g-svg';
+import { Data, objToString, parseCounterData } from '@utils/parse';
 import dayjs from 'dayjs';
 
 let chart: Chart | null = null;
 
 const svgRenderer = new Renderer();
 
-export function drawHeatMap(id: string, metricsData: Result) {
-  log('render heatmap chart...');
-
-  chart = new Chart({
+function createChart(id: string) {
+  return new Chart({
     container: id,
     theme: 'classic',
     autoFit: true,
     renderer: svgRenderer,
     marginBottom: 20,
   });
+}
+
+export function drawHeatMap(id: string, metricsData: Result) {
+  log('render heatmap chart...');
+
+  chart = createChart(id);
 
   chart
     .rect()
@@ -74,29 +79,36 @@ export function drawHeatMap(id: string, metricsData: Result) {
   chart.render();
 }
 
-export function drawLines() {
-  // if (chart !== null) {
-  //   log('render lines chart...');
-  //   chart
-  //     .line()
-  //     .data({
-  //       type: 'fetch',
-  //       value: 'https://assets.antv.antgroup.com/g2/indices.json',
-  //     })
-  //     .transform({ type: 'normalizeY', basis: 'first', groupBy: 'color' })
-  //     .encode('x', (d: any) => new Date(d.Date))
-  //     .encode('y', 'Close')
-  //     .encode('color', 'Symbol')
-  //     .scale('y', { type: 'log' })
-  //     .axis('y', { title: '↑ Change in price (%)' })
-  //     .label({
-  //       text: 'Symbol',
-  //       selector: 'last',
-  //       style: {
-  //         fontSize: 10,
-  //       },
-  //     })
-  //     .tooltip({ channel: 'y', valueFormatter: '.1f' });
-  //   chart.render();
-  // }
+export function drawCounter(id: string, metricsData: Result) {
+  chart = createChart(id);
+
+  chart
+    .line()
+    .data(parseCounterData(metricsData))
+    .encode('x', 'date')
+    .encode('y', 'count')
+    .encode('color', (d: Data) => objToString(d.metrics))
+    .axis('y', { title: false })
+    .axis('x', {
+      title: '时间',
+      titlePosition: 'bottom',
+      label: {
+        autoHide: true,
+      },
+      labelFormatter: (e: number) => dayjs(e).format('YYYY-MM-DD HH:mm'),
+    })
+    .tooltip({
+      title: (_, idx, __, column) => {
+        return dayjs(column.x.value[idx!]).format('YYYY-MM-DD HH:mm:ss');
+      },
+    })
+    .legend('color', false);
+
+  chart.render();
+}
+
+export function updateCounterData(metricsData: Result) {
+  if (chart !== null) {
+    chart.changeData(parseCounterData(metricsData));
+  }
 }
