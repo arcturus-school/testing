4126ba6e2afe31eef012d55c64b0a85c4e13edd8
ARCTRUS <1638330246@qq.com>
2023-05-25 18:53:11 +0800

    feat: Add parseCounterData

---
+++ src/web/src/utils/parse.ts
@@ -0,0 +1,26 @@
+export interface Data {
+  [key: string]: string | number;
+}
+
+export function parseCounterData(data: Result): Data[] {
+  return data.result.flatMap((e) => {
+    const m: any = Object.assign({}, e.metric);
+
+    // 去掉一些无所谓的数据
+    delete m.__name__;
+    delete m.job;
+    delete m.instance;
+
+    return e.values.map((v) => {
+      return {
+        metrics: m,
+        date: v[0] * 1000,
+        count: Number(v[1]),
+      };
+    });
+  });
+}
+
+export function objToString(m: any) {
+  return JSON.stringify(m);
+}
