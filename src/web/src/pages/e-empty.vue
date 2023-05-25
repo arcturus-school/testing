f7c7b4dd4cab65c6547960514865b316a0c13d99
ARCTRUS <1638330246@qq.com>
2023-05-25 10:18:50 +0800

    feat: Add empty page

---
+++ src/web/src/pages/e-empty.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper center">
+    <a-empty :image="Empty.PRESENTED_IMAGE_SIMPLE" description="暂无数据" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Empty } from 'ant-design-vue';
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  flex: 1;
+}
+</style>
